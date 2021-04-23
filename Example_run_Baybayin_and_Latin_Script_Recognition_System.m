c3f221b73d2f03ecb64a9be386c5131079737236
rbp0803 <73572677+rbp0803@users.noreply.github.com>
2021-04-23 15:10:01 +0800

    Add files via upload

---
+++ Example_run_Baybayin_and_Latin_Script_Recognition_System.m
@@ -0,0 +1,12 @@
+%Example script to implement the Proposed Baybayin and Latin Script Recognition System
+%Copy the command to the command window then press
+%return. Or, you can simply Run this script.
+
+output=Baybayin_identifier('Example1.png');
+
+%You can also try the other given images.
+
+%output=Baybayin_identifier('Example2.png');
+%output=Baybayin_identifier('Example3.png');
+%output=Baybayin_identifier('Doctrina Christiana Page.PNG');
+
